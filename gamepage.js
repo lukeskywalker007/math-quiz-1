efa47713ded4b5bf1264b53fb4da0e8ed65c4704
lukeskywalker007 <76431909+lukeskywalker007@users.noreply.github.com>
2021-05-04 12:56:53 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,11 @@
+ player1= localStorage.getItem("username1");
+ player2= localStorage.getItem("username2");
+ player1score=0;
+ player2score=0;
+ console.log(player1);
+ console.log(player2);
+ document.getElementById("player1_name").innerHTML=player1;
+ document.getElementById("player2_name").innerHTML=player2;
+ document.getElementById("player1_score").innerHTML=player1score;
+ document.getElementById("player2_score").innerHTML=player2score;
+ 
